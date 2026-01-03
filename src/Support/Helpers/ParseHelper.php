634267503da6dd8092fb3c99ab4d03725280ddf4
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2026-01-04 04:09:34 +0530

    [Fix]: fixing minor issue 4

--- src/Support/Helpers/ParseHelper.php
+++ src/Support/Helpers/ParseHelper.php
@@ -9,46 +9,52 @@ class ParseHelper
     /** @param string $dir */
     public function is_nested($dir): bool
     {
-        return $dir[0] === "{" && $dir[strlen($dir) - 1] === "}";
+        return strlen($dir) > 0 &&
+            $dir[0] === "{" &&
+            $dir[strlen($dir) - 1] === "}";
     }
 
-    /**
-    * @param string $input
-    * @return array{dirs: string, files: string}
-    * */
-    public function parse ($input):  array
+      /**
+     * @parm string $input
+     * @return array{dirs: string[], files: string[]}
+     */
+    public function parse(string $input): array
     {
-        if($this->is_nested($input))
-        {
-            throw new Exception('Wront Input');
+        if ($input === '') {
+            throw new Exception('Input cannot be empty');
         }
-        $dir_and_file_array = explode("/", $input);
-        if(count($dir_and_file_array) !== 2)
-        {
-            throw new Exception('Not Supported');
-        }
-        [$dirs, $files] = $dir_and_file_array;
-        $result = [];
-        $dir_name = "dirs";
-        $file_name = "files";
 
-        if($this->is_nested($dirs))
-        {
-            $list_of_dir = $this->parse_string($dirs);
-            $result[$dir_name] = $list_of_dir;
-        }else {
-            $result[$dir_name] = [$dirs];
+        if ($this->is_nested($input)) {
+            throw new Exception('Whole input cannot be nested');
         }
-        if($this->is_nested($files))
-        {
-            $list_of_files = $this->parse_string($files);
-            $result[$file_name] = $list_of_files;
-        } else{
-            $result[$file_name] = [$files];
+
+        $parts = explode('/', $input);
+
+        if (count($parts) > 2) {
+            throw new Exception('Nested paths not supported');
         }
-        return $result;
+
+        [$dirs, $files] = array_pad($parts, 2, '');
+
+        return [
+            'dirs'  => $this->parsePart($dirs),
+            'files' => $this->parsePart($files),
+        ];
     }
 
+      /** @return string[] */
+    private function parsePart(string $part): array
+    {
+        if ($part === '') {
+            return [];
+        }
+
+        if ($this->is_nested($part)) {
+            return $this->parse_string($part);
+        }
+
+        return [$part];
+    }
 
     /** @return string[] */
     public function parse_string (string $input): array
